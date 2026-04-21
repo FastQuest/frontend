0b0726210b08dd7d787b0625242c282938e0efe9
Anthony Passos <anthony.passos@sempreceub.com>
2026-04-21 16:20:43 -0300

    responsividade home primeira seção

--- src/components/HomeMain.vue
+++ src/components/HomeMain.vue
@@ -1,7 +1,17 @@
 <template>
-    <main class="w-full h-[70vh] bg-[url('/public/imgs/home/bg-home.png')] bg-cover flex items-center px-16">
-        <p class="text-black text-6xl lg:w-2/3 font-light text-end md:text-start">
+    <main
+      class="w-full flex items-center relative h-[60vh] min-h-[20rem] md:min-h-[30rem] md:h-[70vh]"
+    >
+      <img
+        src="/public/imgs/home/bg-home.png"
+        alt="Fundo da seção"
+        class="md:w-screen h-full object-cover object-left"
+      >
+
+      <div class="absolute inset-0 flex items-center px-8 md:px-16 w-full">
+        <p class="text-black text-3xl md:text-6xl font-light">
           <b>PREPARE-SE</b> PARA <br> OS <b>DESAFIOS</b> QUE <br>EXIGEM O <br> <b>MELHOR DE VOCÊ</b>
         </p>
+      </div>
     </main>
 </template>
