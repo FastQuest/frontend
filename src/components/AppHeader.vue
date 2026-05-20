cb2ade80f2cf56955d5e5508649f83960ea1bcb7
guilhermemagarao <guilherme.magarao@sempreceub.com>
2026-05-20 19:19:36 -0300

    Adicionando Icone do Usuario ao header.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,12 +4,20 @@
 <template>
     <header id="header" class="bg-black flex justify-between items-center w-full p-6 md:px-12 select-none">
         <img class="h-10" src="/public/imgs/logo.png" alt="">
-        <ul class="text-white text-xl font-normal hidden sm:flex">
-          <li class="hover:cursor-pointer "><router-link class="p-2" to="/">Home</router-link></li>
-          <li class="hover:cursor-pointer "><router-link class="p-2" to="/questions">Questões</router-link></li>
-          <li class="hover:cursor-pointer "><router-link class="p-2" to="/lists">Pastas</router-link></li>
-          <li class="hover:cursor-pointer "><router-link class="p-2" to="/create">Criar</router-link></li>
+        
+        <ul class="text-white text-xl font-normal hidden sm:flex items-center">
+          <li class="hover:cursor-pointer"><router-link class="p-2" to="/">Home</router-link></li>
+          <li class="hover:cursor-pointer"><router-link class="p-2" to="/questions">Questões</router-link></li>
+          <li class="hover:cursor-pointer"><router-link class="p-2" to="/lists">Pastas</router-link></li>
+          <li class="hover:cursor-pointer"><router-link class="p-2" to="/create">Criar</router-link></li>
+          
+          <li class="hover:cursor-pointer ml-4">
+            <button type="button" @click="() => {}" class="flex items-center focus:outline-none">
+                <img src="/public/imgs/header/user_icon.svg" class="h-10" alt="" />
+            </button>
+          </li>
         </ul>
+        
         <img src="/public/imgs/icon_menu.svg" class="h-10 sm:hidden"/>
     </header>
 </template>
